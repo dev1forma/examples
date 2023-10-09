0b5f400efe6d8b7942b89700605cac973e129837
dev1forma <136583901+dev1forma@users.noreply.github.com>
2023-10-09 14:50:07 +0000

    +

--- spa-custom-resources/test-js.js
+++ spa-custom-resources/test-js.js
@@ -1,11 +1,25 @@
 /**
  * 
- * add to config
+ * add to custom settings config
  * 
+ * "spaResources": [
+    {
+      "type": "js",
+      "src": "https://cdn.jsdelivr.net/gh/dev1forma/examples/spa-custom-resources/test-js.js"
+    }
+  ]
  */
 
 ;(() => {
 
     console.log('hello from custom initializer script')
+
+    spaEvent('init', e => {
+        console.log('spa init');
+    });
+
+    spaEvent('auth', e => {
+        console.log('auth', e);
+    });
 
 })();
