9d75ce54730e075f70d229c382994e0a8b33ca1c
stacktracer <vostroknutov@hotmail.com>
2024-11-07 23:43:34 +0500

    +

---
+++ spa-custom-help-widget/v2/index.js
@@ -0,0 +1,14 @@
+import('https://cdn.jsdelivr.net/gh/dev1forma/examples/spa-custom-help-widget/v1/lib.esm.js').then(({ init }) => {
+    const root = document.createElement('div');
+    document.body.append(root);
+
+    const link = document.createElement('link');
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "https://cdn.jsdelivr.net/gh/dev1forma/examples/spa-custom-help-widget/v1/lib.esm.css";
+    document.head.append(link);
+
+    init({
+        container: root,
+    });
+});
