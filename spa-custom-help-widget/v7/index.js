c8d108d106d445740dfa55a5a9b0d30aacb341ac
stacktracer <vostroknutov@hotmail.com>
2024-11-18 15:37:29 +0500

    +

---
+++ spa-custom-help-widget/v7/index.js
@@ -0,0 +1,22 @@
+(() => {
+
+    const libUrl = 'https://cdn.jsdelivr.net/gh/dev1forma/examples/spa-custom-help-widget/v7/lib.esm.js',
+        cssUrl = 'https://cdn.jsdelivr.net/gh/dev1forma/examples/spa-custom-help-widget/v7/lib.esm.css';
+
+
+    import(libUrl).then(({ init }) => {
+        const root = document.createElement('div');
+        document.body.append(root);
+
+        const link = document.createElement('link');
+        link.rel = "stylesheet";
+        link.type = "text/css";
+        link.href = cssUrl;
+        document.head.append(link);
+
+        init({
+            container: root,
+        });
+    });
+
+})();
